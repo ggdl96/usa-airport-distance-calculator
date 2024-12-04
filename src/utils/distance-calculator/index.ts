de8005b909ae2a6fd4219d5729ef7abc8ae5bd31
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-12-04 13:53:12 -0300

    add functions to calculate distance from 2 points

---
+++ src/utils/distance-calculator/index.ts
@@ -0,0 +1,27 @@
+type Coordinates = {
+  lat: number;
+  lng: number;
+};
+
+export const caclculateDistanceBetweenTwoPoints = (
+  coordinatesA: Coordinates,
+  coordinatesB: Coordinates
+) => {
+  const toRad = (value: number) => (value * Math.PI) / 180;
+
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = toRad(coordinatesB.lat - coordinatesA.lat);
+  const dLng = toRad(coordinatesB.lng - coordinatesA.lng);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(coordinatesA.lat)) *
+      Math.cos(toRad(coordinatesB.lat)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in kilometers
+};
+
+export const kmsToMiles = (distanceInKM: number) => distanceInKM * 0.5399565
