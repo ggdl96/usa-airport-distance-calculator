11054d539ea41a1790e032290b0fa78ff1579842
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-12-05 11:33:16 -0300

    removed github from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -187,16 +187,6 @@ export default async function Home() {
         <h1 className="text-3xl">USA Airport distance calculator</h1>
         <DistanceCalculator airportList={airportList} />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/ggdl96/usa-airport-distance-calculator"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </footer>
     </div>
   );
 }
