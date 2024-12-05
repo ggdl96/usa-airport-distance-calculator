a84901dee7ca8ee364d182baa5e712fb5847d65c
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-12-05 10:59:35 -0300

    improve test description

--- src/utils/airport/airport.spec.ts
+++ src/utils/airport/airport.spec.ts
@@ -1,7 +1,7 @@
 import { getNameFromAirportCode } from ".";
 
 describe("getNameFromAirportCode", () => {
-  test("given x should return y", () => {
+  test("given MDQ iata (airport code) should return 'Astor Piazzolla  International Airport' name", () => {
     const airportList = [
       {
         code: "MDQ",
@@ -15,6 +15,6 @@ describe("getNameFromAirportCode", () => {
 
     const name = getNameFromAirportCode(airportList, "MDQ");
 
-    expect(name).toBe(name);
+    expect(name).toBe("Astor Piazzolla  International Airport");
   });
 });
