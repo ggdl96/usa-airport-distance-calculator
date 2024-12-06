404fbbbcb2b3d4fe7489ad8d6a2d59cbf4b177e8
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2024-12-06 10:52:51 -0300

    update logic to zoom map

--- src/components/map-display/index.tsx
+++ src/components/map-display/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, Marker, Polyline } from "@react-google-maps/api";
 import { Coordinates } from "@/types/Geo";
 import { containerStyle, labelStyles, lineStyles } from "./styles";
@@ -18,19 +18,59 @@ const MapDisplay = ({
   destinationName: string;
   originName: string;
 }) => {
+  const [options, setOptions] = useState({
+    minZoom: 3,
+    maxZoom: 7,
+  });
+
+  useEffect(() => {
+    const updateZoom = () => {
+      if (window.matchMedia("(max-width: 640px)").matches) {
+        setOptions({
+          minZoom: 3,
+          maxZoom: 7,
+        });
+      } else if (window.matchMedia("(max-width: 768px)").matches) {
+        setOptions({
+          minZoom: 3,
+          maxZoom: 8,
+        });
+      } else if (window.matchMedia("(max-width: 1024px)").matches) {
+        setOptions({
+          minZoom: 4,
+          maxZoom: 9,
+        });
+      } else {
+        setOptions({
+          minZoom: 5,
+          maxZoom: 12,
+        });
+      }
+    };
+
+    updateZoom();
+    window.addEventListener("resize", updateZoom);
+
+    return () => {
+      window.removeEventListener("resize", updateZoom);
+    };
+  }, []);
+
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={options.minZoom}
+      options={options}
+    >
       <Marker position={origin} label={{ ...labelStyles, text: originName }} />
       <Marker
         position={destination}
         label={{ ...labelStyles, text: destinationName }}
       />
-      <Polyline
-        path={[origin, destination]}
-        options={lineStyles}
-      />
+      <Polyline path={[origin, destination]} options={lineStyles} />
     </GoogleMap>
   );
 };
